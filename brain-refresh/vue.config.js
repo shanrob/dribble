01d93490bfbab413bb5eb6b4d4051b475fc96481
Shannon Robinson <srobin4922@gmail.com>
2024-06-24 14:09:55 -0400

    Changing name of build dir from dist to build

--- brain-refresh/vue.config.js
+++ brain-refresh/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+
+const path = require("path")
+
 module.exports = defineConfig({
+  outputDir: path.resolve(__dirname, "./build"),
   transpileDependencies: true,
 });
