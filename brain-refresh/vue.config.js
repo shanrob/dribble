c6d16f52a9c0dab1b7144d12c86bfa55ba66fabe
Shannon Robinson <srobin4922@gmail.com>
2024-07-19 11:52:24 -0400

    cleanup

--- brain-refresh/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-
-const path = require("path")
-
-module.exports = defineConfig({
-  outputDir: path.resolve(__dirname, "./build"),
-  transpileDependencies: true,
-});
